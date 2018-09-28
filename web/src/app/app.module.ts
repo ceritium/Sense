e2072dd34b78ac28f0b0d63f62fa84a16514a70e
David Martín García <davidmartingarcia0@gmail.com>
2018-09-28 10:52:21 +0200

    basic stuff in front

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -16,25 +16,29 @@ import { DeviceSearchComponent }  from './device-search.component';
 import { MetricComponent }      from './metric.component';
 import { MetricDetailComponent }      from './metric-detail.component';
 import { MeasureComponent }      from './measure.component';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    AppRoutingModule
-  ],
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    DeviceDetailComponent,
-    DevicesComponent,
-    DeviceSearchComponent,
-    MetricComponent,
-    MetricDetailComponent,
-    MeasureComponent
-  ],
-  providers: [ DeviceService, MetricService, MeasureService ],
-  bootstrap: [ AppComponent ]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        AppRoutingModule,
+        MaterialModule,
+        BrowserAnimationsModule
+    ],
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        DeviceDetailComponent,
+        DevicesComponent,
+        DeviceSearchComponent,
+        MetricComponent,
+        MetricDetailComponent,
+        MeasureComponent
+    ],
+    providers: [ DeviceService, MetricService, MeasureService ],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
