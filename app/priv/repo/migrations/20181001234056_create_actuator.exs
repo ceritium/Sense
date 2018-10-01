f5e24165b096bc213fa4a7b3dd6fc1315ebcf720
David Martin Garcia <base.dks@gmail.com>
2018-10-01 23:39:35 +0200

    create actuator models

---
+++ app/priv/repo/migrations/20181001234056_create_actuator.exs
@@ -0,0 +1,17 @@
+defmodule Sense.Repo.Migrations.CreateActuator do
+  use Ecto.Migration
+
+  def change do
+    create table(:actuators) do
+      add :name, :string
+      add :description, :string
+      add :state, :string
+      add :type, :string
+      add :device_id, references(:devices, on_delete: :delete_all), null: false
+
+      timestamps()
+    end
+    create index(:actuators, [:device_id])
+
+  end
+end
---
+++ web/src/app/actuator.ts
@@ -0,0 +1,8 @@
+export class Actuator {
+  id: number;
+  name: string;
+  description: string;
+  state: string
+  type: string
+  device_id: number;
+}
